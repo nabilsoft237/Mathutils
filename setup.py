92ca9cde2a8fb6ffc460290a411d1bb446b8a353
nabilsoft237 <nabilf045@mail.com>
2025-03-20 12:39:48 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='Mathutils',
+    version='1.0',
+    description='Un package python pour le operations mahematiques courantes',
+    author='ngouhouo',
+    author_email='nabilf045@gmail.com',
+    packages=['mahtutils'],
+    install_requires=['numpy']
+)
